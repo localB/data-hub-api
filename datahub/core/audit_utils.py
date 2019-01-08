0f0433409dee8d1766a255d2ba796f70a11c12f9
Marcus Patino Pan <work@Marcuss-MacBook-Pro.local>
2019-01-08 14:48:51 +0000

    fixup - PR fixes

--- datahub/core/audit_utils.py
+++ datahub/core/audit_utils.py
@@ -2,21 +2,23 @@ from django.core.exceptions import ValidationError
 from django.db import models
 
 
-def diff_versions(model_meta_data, old_version, new_version):
-    """Audit versions comparision with the delta returned."""
-    changes = {}
+def diff_versions(model_meta, old_version, new_version):
+    """
+    Audit versions comparision with the delta returned.
+
+    For related objects only the pk is stored in the audit history.
+    A user friendly representation of the related object (the object name)
+    is retrieved if the relationship still exists.
+
+    """
+    friendly_changes = {}
     raw_changes = _get_changes(old_version, new_version)
 
     for db_field_name, values in raw_changes.items():
-        field = _get_db_field_from_column_name(model_meta_data, db_field_name)
-        if field and list(filter(is_not_empty_or_none, values)):
-            changes[field.name] = [_get_value_for_field(field, value) for value in values]
-    return changes
-
-
-def is_not_empty_or_none(value):
-    """Checks if value is not empty or none."""
-    return value not in ('', None)
+        field = _get_field_or_none(model_meta, db_field_name)
+        field_name = field.name if field else db_field_name
+        friendly_changes[field_name] = [_get_value_for_field(field, value) for value in values]
+    return friendly_changes
 
 
 def _get_changes(old_version, new_version):
@@ -28,36 +30,55 @@ def _get_changes(old_version, new_version):
             changes[field_name] = [None, new_value]
         else:
             old_value = old_version[field_name]
-            if old_value != new_value:
+            if _are_values_different(old_value, new_value):
                 changes[field_name] = [old_value, new_value]
     return changes
 
 
-def _get_db_field_from_column_name(model_meta_data, db_column_name):
-    """Gets django db field for a given column name"""
+def _are_values_different(old_value, new_value):
+    """Checks if the two values are different whilst treating a blank string the same as a None."""
+    old_value = old_value if old_value != '' else None
+    new_value = new_value if new_value != '' else None
+    return old_value != new_value
+
+
+def _get_field_or_none(model_meta, db_column_name):
+    """Gets a model field for a given model meta, if the field cannot be found returns None."""
     try:
-        return model_meta_data.get_field(db_column_name)
+        return model_meta.get_field(db_column_name)
     except models.FieldDoesNotExist:
         return None
 
 
 def _get_value_for_field(field, value):
-    """Checks field type and if required retrieves friendly value from related model."""
-    if not field.is_relation:
+    """
+    Checks field and if required retrieves the object name from related model.
+
+    If the field is None or not a related field then the value can be
+    returned as the value is not an object id (pk).
+
+    For related objects the object name is then retrieved, for many to many and
+    one to many all values need to be retrieved individually.
+
+    """
+    if not field or not field.is_relation:
         return value
+
     if field.many_to_many or field.one_to_many:
         return [
-            _get_friendly_repr_of_object_reference(
+            _get_object_name_for_pk(
                 field.related_model, one_value,
             ) for one_value in value
         ]
-    return _get_friendly_repr_of_object_reference(field.related_model, value)
+    return _get_object_name_for_pk(field.related_model, value)
 
 
-def _get_friendly_repr_of_object_reference(db_model, value):
-    """Gets friendly representation for a given object reference."""
+def _get_object_name_for_pk(model, pk):
+    """
+    Gets the name for a given object pk or returns the pk if it cannot be found.
+    """
     try:
-        result = db_model.objects.get(pk=value)
-    except (db_model.DoesNotExist, ValueError, TypeError, ValidationError):
-        return value
+        result = model.objects.get(pk=pk)
+    except (model.DoesNotExist, ValueError, TypeError, ValidationError):
+        return pk
     return str(result)
--- datahub/core/test/test_audit_utils.py
+++ datahub/core/test/test_audit_utils.py
@@ -1,15 +1,14 @@
 import unittest.mock
 
 import pytest
-from django.db import models
 
 from datahub.core.audit_utils import (
+    _are_values_different,
     _get_changes,
-    _get_db_field_from_column_name,
-    _get_friendly_repr_of_object_reference,
+    _get_field_or_none,
+    _get_object_name_for_pk,
     _get_value_for_field,
     diff_versions,
-    is_not_empty_or_none,
 )
 from datahub.core.test.support.models import Book
 from datahub.core.test.support.factories import BookFactory
@@ -21,23 +20,26 @@ def test_audit_diff_versions():
     """Test audit diff versions."""
     given = {
         'old': {
-            'date_publish': 'val1',
+            'date_published': 'val1',
             'name': 'val2',
             'contact_email': 'contact',
             'old_email': 'hello',
             'telephone_number': None,
+            'old_field': '1',
         },
         'new': {
             'date_published': 'val1',
             'name': 'new-val',
             'genre': 'added',
             'telephone_number': '',
+            'old_field': '2',
         },
     }
 
     expected = {
         'name': ['val2', 'new-val'],
         'genre': [None, 'added'],
+        'old_field': ['1', '2'],
     }
 
     result = diff_versions(Book._meta, given['old'], given['new'])
@@ -45,18 +47,18 @@ def test_audit_diff_versions():
 
 
 @pytest.mark.parametrize(
-    'value,expected_result',
+    'old_value,new_value,expected_result',
     (
-        (None, False),
-        ('', False),
-        (False, True),
-        ([], True),
-        ('hello', True),
+        ('', None, False),
+        ('', False, True),
+        ('hello', 'hello', False),
+        (None, None, False),
+        ('', '', False),
     ),
 )
-def test_is_not_empty_or_none(value, expected_result):
-    """Tests if value is not empty or none."""
-    assert is_not_empty_or_none(value) == expected_result
+def test_are_values_different(old_value, new_value, expected_result):
+    """Tests two values are different but that a blank string is treated as a None."""
+    assert _are_values_different(old_value, new_value) == expected_result
 
 
 @pytest.mark.parametrize(
@@ -65,46 +67,12 @@ def test_is_not_empty_or_none(value, expected_result):
         ({}, {}, {}),
         ({}, {'hello': 1}, {'hello': [None, 1]}),
         ({'hello': 1}, {}, {}),
-        ({'hello': None}, {'hello': ''}, {'hello': [None, '']}),
+        ({'hello': None}, {'hello': ''}, {}),
     ),
 )
 def test_get_changes(old_version, new_version, expected_result):
     """Tests get changes between two dictionaries"""
     assert _get_changes(old_version, new_version) == expected_result
-
-
-@pytest.mark.parametrize(
-    'column_name,expected_type',
-    (
-        ('hello', type(None)),
-        ('name', models.TextField),
-        ('proofreader', models.ForeignKey),
-    ),
-)
-def test_get_db_field_from_column_name(column_name, expected_type):
-    """Test getting the db field for a given column name."""
-    result = _get_db_field_from_column_name(Book._meta, column_name)
-    assert type(result) is expected_type
-
-
-def test_get_friendly_repr_of_object_reference_for_existing_object():
-    """Test getting representation of existing object."""
-    book = BookFactory()
-    assert _get_friendly_repr_of_object_reference(Book, book.pk) == str(book)
-
-
-@pytest.mark.parametrize(
-    'value',
-    (
-        'hello',
-        'c33f4ce3-051a-11e9-aa56-c82a140516f8',
-        1,
-        [],
-    ),
-)
-def test_get_friendly_repr_of_object_reference_returns_value_when_no_object_found(value):
-    """Test getting representation for an object that no longer exists."""
-    assert _get_friendly_repr_of_object_reference(Book, value) == value
 
 
 @pytest.mark.parametrize(
@@ -115,8 +83,8 @@ def test_get_friendly_repr_of_object_reference_returns_value_when_no_object_foun
         ('authors', ['value1', 'value2'], ['fake', 'fake'], 2),
     ),
 )
-@unittest.mock.patch('datahub.core.audit_utils._get_friendly_repr_of_object_reference')
-def test_get_value_for_field_(
+@unittest.mock.patch('datahub.core.audit_utils._get_object_name_for_pk')
+def test_get_value_for_field(
     mock_get_repr, field_name, values, expected_result, number_of_times_get_repr_called,
 ):
     """
@@ -124,7 +92,29 @@ def test_get_value_for_field_(
     Tests foreign key, many to many and char fields.
     """
     mock_get_repr.return_value = 'fake'
-    field = _get_db_field_from_column_name(Book._meta, field_name)
+    field = _get_field_or_none(Book._meta, field_name)
     result = _get_value_for_field(field, values)
     assert mock_get_repr.call_count == number_of_times_get_repr_called
     assert result == expected_result
+
+
+class TestGetFriendlyReprOfObjectReference:
+    """Tests for get friendly repr of object reference"""
+
+    def test_get_repr_for_existing_object(self):
+        """Test getting representation of existing object."""
+        book = BookFactory()
+        assert _get_object_name_for_pk(Book, book.pk) == str(book)
+
+    @pytest.mark.parametrize(
+        'value',
+        (
+            'hello',
+            'c33f4ce3-051a-11e9-aa56-c82a140516f8',
+            1,
+            [],
+        ),
+    )
+    def test_get_repr_when_no_object_found_returns_value(self, value):
+        """Test getting representation for an object that no longer exists."""
+        assert _get_object_name_for_pk(Book, value) == value
