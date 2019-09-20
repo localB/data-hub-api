63de80a2a035d7dabf950171b840898ed0579099
Brendan Smith <brendan.smith@gamer-network.net>
2019-09-20 13:08:45 +0100

    Spike for dataset endpoint using serializer

--- datahub/dataset/views.py
+++ datahub/dataset/views.py
@@ -1,3 +1,6 @@
+from django.db.models import F
+from rest_framework import generics
+from rest_framework import serializers
 from rest_framework.views import APIView
 
 from config.settings.types import HawkScope
@@ -81,7 +84,36 @@ class OMISDatasetView(HawkResponseSigningMixin, APIView):
         )
 
 
-class ContactsDatasetView(HawkResponseSigningMixin, APIView):
+class ContactDatasetSerializer(serializers.ModelSerializer):
+    
+    company_sector = serializers.CharField()
+    company_number = serializers.CharField()
+    company_name = serializers.CharField()
+    company_uk_region_name = serializers.CharField()
+
+    class Meta:
+        model = Contact
+        fields = [
+            'id',
+            'accepts_dit_email_marketing',
+            'address_postcode',
+            'company_number',
+            'company_name',
+            'company_uk_region_name',
+            'company_sector',
+            'created_on',
+            'email',
+            'email_alternative',
+            'job_title',
+            'notes',
+            'telephone_alternative',
+            'telephone_number',
+            'first_name',
+            'last_name',
+        ]
+
+
+class ContactsDatasetView(generics.ListAPIView):
     """
     An APIView that provides 'get' action which queries and returns desired fields for
     Contacts Dataset to be consumed by Data-flow periodically. Data-flow uses response result
@@ -94,33 +126,16 @@ class ContactsDatasetView(HawkResponseSigningMixin, APIView):
     permission_classes = (HawkScopePermission, )
     required_hawk_scope = HawkScope.data_flow_api
     pagination_class = ContactsDatasetViewCursorPagination
+    serializer_class = ContactDatasetSerializer
 
-    def get(self, request):
-        """Endpoint which serves all records for Contacts Dataset"""
-        dataset = self.get_dataset()
-        paginator = self.pagination_class()
-        page = paginator.paginate_queryset(dataset, request, view=self)
-        return paginator.get_paginated_response(page)
-
-    def get_dataset(self):
-        """Returns list of Contacts Dataset records"""
-        return Contact.objects.annotate(
-            name=get_full_name_expression(),
+    def get_queryset(self):
+        return Contact.objects.all().annotate(
+            company_number=F('company__company_number'),
+            company_name=F('company__name'),
+            company_uk_region_name=F('company__uk_region__name'),
             company_sector=get_sector_name_subquery('company__sector'),
-        ).values(
-            'accepts_dit_email_marketing',
-            'address_country__name',
-            'address_postcode',
-            'company__company_number',
-            'company__name',
-            'company__uk_region__name',
-            'company_sector',
-            'created_on',
-            'email',
-            'email_alternative',
-            'job_title',
-            'name',
-            'notes',
-            'telephone_alternative',
-            'telephone_number',
+        ).select_related(
+            'company',
+            'company__uk_region',
+            'company__sector',
         )
